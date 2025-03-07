fd4e40c911dd1b705b201e1604d413f60c80339c
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2025-03-07 23:02:36 +0530

    Update ResetPassword.jsx

--- client/src/pages/ResetPassword.jsx
+++ client/src/pages/ResetPassword.jsx
@@ -15,7 +15,7 @@ const ResetPassword = () => {
     e.preventDefault();
     await axios
       .put(
-        `http://localhost:4000/api/v1/user/password/reset/${token}`,
+        `https://finance-web-5s0d.onrender.com/api/v1/user/password/reset/${token}`,
         { password, confirmPassword },
         {
           withCredentials: true,
