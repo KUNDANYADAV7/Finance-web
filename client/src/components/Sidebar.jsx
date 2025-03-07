b8eeb4efa00b4dab8c654e032325c17cc5eeb450
KUNDANYADAV7 <141399895+KUNDANYADAV7@users.noreply.github.com>
2025-03-07 23:16:18 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -30,7 +30,7 @@ const Sidebar = () => {
 
   const handleLogout = async () => {
     try {
-      const res = await axios.get("http://localhost:4000/api/v1/user/logout", {
+      const res = await axios.get("https://finance-web-5s0d.onrender.com/api/v1/user/logout", {
         withCredentials: true,
       });
 
